f0dbd840cb6502fc14ef2b80b29302726eb9f45f
OBEY <obeyblessing@gmail.com>
2024-10-24 14:58:00 +0200

    v1 commit

---
+++ src/test/java/xyz/obeyblessingportfolio/ObeyblessingportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.obeyblessingportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObeyblessingportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
